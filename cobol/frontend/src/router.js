e5f6c461613a8d748b575bd67c670c444150654d
nguyenhat3105 <169120327+nguyenhat3105@users.noreply.github.com>
2025-11-02 11:39:32 +0700

    Update router.js

--- cobol/frontend/src/router.js
+++ cobol/frontend/src/router.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+// THÊM CÁC IMPORT NÀY
+import SearchFlightPage from './pages/SearchFlightPage';
+import BookingStep1 from './pages/BookingStep1';
+// (Chúng ta cũng nên thêm một trang cho BookingStep2, dựa trên sell2-1.png)
 
 export default function Router(){
   return (
     <Routes>
       <Route path="/" element={<LoginPage/>} />
+      {/* THÊM CÁC ROUTE NÀY */}
+      <Route path="/search" element={<SearchFlightPage />} />
+      <Route path="/booking-step1" element={<BookingStep1 />} />
+      {/* <Route path="/booking-step2" element={<BookingStep2 />} /> */}
     </Routes>
   )
 }
+
